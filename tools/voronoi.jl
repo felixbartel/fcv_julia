9abf1c65b8f66c0235d70af72a8df442f8530166
felixbartel <felixbartel@protonmail.com>
2020-01-10 14:52:39 +0100

    fixed bug

--- tools/voronoi.jl
+++ tools/voronoi.jl
@@ -6,7 +6,7 @@ function voronoiArea(nodes)
   if d == 1
     p = sortperm(nodes)
     nodes_sorted = nodes[p]
-    w = ([nodes_sorted[2:end]; nodes[1]+1]-[nodes_sorted[end]-1; nodes_sorted[1:end-1]])./2
+    w = ([nodes_sorted[2:end]; nodes_sorted[1]+1]-[nodes_sorted[end]-1; nodes_sorted[1:end-1]])./2
     return w[invperm(p)]
   elseif d == 2
     shift = -ones(d)
